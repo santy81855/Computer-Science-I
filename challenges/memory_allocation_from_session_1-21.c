7213d64a41b7e62d19c1e0b6f2ca72a3c1e987d3
Santy <mynameissantiagogarcia@gmail.com>
2021-01-21 15:19:52 -0500

    Finished the classroom and chairs example from session 1-21. Includes frees

--- challenges/memory_allocation_from_session_1-21.c
+++ challenges/memory_allocation_from_session_1-21.c
@@ -79,5 +79,27 @@ int main()
     // And lastly, we free the classroom pointer
     free(cb1);
 
+    /*
+    Now for some normal talk, we will just conceptually explain what we did in this code:
+
+    We have a single classroom. We could have made it static (not a pointer), but we made it a pointer
+    so that we could get some practice allocating. Inside of a classroom struct we have a double pointer
+    of the Chair struct. This represents that we can have multiple chairs (an array) inside of each classroom.
+    This could have been a single pointer and it would have worked out just as well, but we wanted it to be 
+    more similar to the HW1 assignment, which involves a double pointer inside of a struct.
+
+    Since we made it a double pointer, we have 2 allocations to take care of. The first one is where we make an 
+    array of single chair pointers, and then the second one is where we allocate a single chair struct into each of those
+    chair pointers that we created. As an aside, we could have given each of our n chair pointers enough space to store 
+    multiple chairs themselves, which could potentially be useful for other programs, but in this one we just needed n 
+    chairs total.
+
+    Once we allocated all of the chair struct pointers to be able to store a single chair struct inside, we then had to 
+    allocate enough memory to store each color as a character array inside of each chair struct. The reason we had to 
+    do this was because we made the color variable inside of the chair struct a pointer. We could have made it static 
+    like this: color[21]
+    That would have made it so we would not have to allocate memory for each color inside of each chair, but again, we 
+    did it this way to get more practice for the tricky homework assignments.
+    */
     return 0;
 }
